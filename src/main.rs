8bf5ac2704439fe5b98449894f0c88db7fcd3faa
Dominik Kondrot <dominik.kondrot@gmail.com>
2023-03-28 18:32:21 +0200

    Chat websocket implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+
+    let listener = TcpListener::bind("localhost:8080")
+        .await
+        .expect("Failed to bind to localhost:8080");
+
+    let (tx, _rx) = broadcast::channel(10);
+
+    loop {
+
+        let (mut socket, addr) = listener.accept()
+            .await
+            .expect("Failed to accept connection");
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (read, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(read);
+
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    },
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
